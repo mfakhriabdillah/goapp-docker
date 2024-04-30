ad9ca1e8b581594a52da5155ca4018f62de22ebc
Muhammad Fakhri Abdillah <fakhriabdillah37@gmail.com>
2024-04-30 13:22:12 +0700

    add cicd workflows

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:alpine3.16 AS build
+WORKDIR /app
+COPY go.mod ./
+RUN go mod download
+COPY go.sum ./
+COPY *.go ./
+RUN go build -o /studidevops
+
+FROM golang:alpine3.16
+WORKDIR /
+COPY --from=build /studidevops /studidevops
+EXPOSE 80
+ENTRYPOINT ["/studidevops"]
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":         "ok",
+			"app_version":     "2.0",
+			"app_description": "deploy app version 2.0 build 2",
+		})
+	})
+	r.GET("/env", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":     200,
+			"data_env": os.Environ(),
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
